e8a8ed55cdfc5f2875d91c365474867c5e44ac8f
Natarajan Selvaraj <natarajan.selvaraj@sincrodigital.com>
2023-09-27 01:01:24 -0400

    Initial Commit

---
+++ src/main/java/com/superwhy/Main.java
@@ -0,0 +1,27 @@
+package com.superwhy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        logger.info ("Printing Log Info Statement !!!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
